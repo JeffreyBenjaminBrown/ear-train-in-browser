3577b8b0f93212d34648b7437663b6ef7f197df9
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2024-05-25 13:49:20 -0500

    start!

---
+++ README.md
@@ -0,0 +1,5 @@
+Compile the .ts file(s) to .js by running
+  `tsc <filename>.ts`
+
+Visit the `index.html` file in Firefox like this:
+  file:///home/jeff/...
---
+++ hello.ts
@@ -0,0 +1,6 @@
+// create a new heading 1 element
+let heading = document.createElement('h1')
+heading.textContent = 'Hello, World!'
+
+// add the heading the document
+document.body.appendChild(heading)
