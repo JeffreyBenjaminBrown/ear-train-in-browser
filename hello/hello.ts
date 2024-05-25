c188cffcb9e6ce9222ce4cc4251b4231ebaad0db
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2024-05-25 17:22:00 -0500

    Now the button works.
      I just had to delete the import statement.

--- hello/hello.ts
+++ hello/hello.ts
@@ -1,13 +1,10 @@
-import * as st from "./simpleTones.js"
-
-// create a title
 let heading = document.createElement('h1')
 heading.textContent = 'Hello, World!'
 document.body.appendChild(heading)
 
 const button = document.createElement('button');
 button.textContent = 'Click me'
-button.addEventListener('click', () =>
+button.addEventListener ( 'click', () =>
     { console.log('Button clicked')
     } )
 document.body.appendChild(button)
